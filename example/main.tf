0ce7b2dac9d61312d2867618b620a8eac4e52401
ac2393921 <ac2393921@gmail.com>
2022-08-07 01:45:40 +0900

    add: example

---
+++ example/main.tf
@@ -0,0 +1,12 @@
+locals {
+  example_instance_type = "t3.micro"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c3fd0f5d33134a76"
+  instance_type = local.example_instance_type
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.id
+}
