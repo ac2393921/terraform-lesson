8922d7d8d3151ab0ae0a26b24fc16656b272dbc0
ac2393921 <ac2393921@gmail.com>
2022-08-08 22:54:07 +0900

    update: use http_server module

--- example/main.tf
+++ example/main.tf
@@ -1,12 +1,8 @@
-locals {
-  example_instance_type = "t3.micro"
+module "web_server" {
+  source        = "./http_server"
+  instance_type = "t3.micro"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = local.example_instance_type
-}
-
-output "example_instance_id" {
-  value = aws_instance.example.id
+output "public_dns" {
+  value = module.web_server.public_dns
 }
